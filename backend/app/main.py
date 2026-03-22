459010dc5e34e6f75b42837acb8a88330b0d8b0f
itskavinkumar <kavinkumars.tech@gmail.com>
2026-03-22 14:12:56 +0530

    fix cors for netlify frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -73,7 +73,7 @@ app.add_middleware(
         "http://localhost:5174",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
-        "https://your-frontend-name.vercel.app"
+        "https://ornate-taiyaki-7440f3.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
